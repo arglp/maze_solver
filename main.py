a70fb85378e434a9ae0ef7da948e98f279609c09
arglp <leopold.prueller@gmx.at>
2024-12-21 17:33:53 +0100

    drawing lines

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from window import Window
+from shapes import Line, Point
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(50, 50), Point(750, 50)), "black")
+    win.draw_line(Line(Point(50, 50), Point(50, 550)), "black")
+    win.draw_line(Line(Point(750, 50), Point(750, 550)), "black")
+    win.draw_line(Line(Point(50, 550), Point(750, 550)), "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from shapes import Line
 
 class Window:
     def __init__ (self, width, height):
@@ -21,5 +22,8 @@ class Window:
     
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
 
 
